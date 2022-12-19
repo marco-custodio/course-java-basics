004b418e7bf95b7e06ae9c0434141c547dc5cf38
Marco Custodio <marcoantoniocp.71@gmail.com>
2022-12-19 18:07:28 -0300

    added exercise-four

---
+++ src/loiane/exercise/ExerciseFour.java
@@ -0,0 +1,28 @@
+package loiane.exercise;
+
+import java.util.Scanner;
+
+public class ExerciseFour {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Type quarterly score");
+
+        System.out.println("First month: ");
+        float first = scan.nextFloat();
+
+        System.out.println("Second month: ");
+        float second = scan.nextFloat();
+
+        System.out.println("Third month: ");
+        float third = scan.nextFloat();
+
+        float average = first + second + third /3;
+
+        System.out.printf("The average score is %.2f", average);
+
+
+
+
+    }
+}

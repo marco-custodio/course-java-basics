0e046abd4d3f18f06f1813c86003e41913e29b24
Marco Custodio <marcoantoniocp.71@gmail.com>
2022-12-19 17:21:50 -0300

    added exercise three

---
+++ src/loiane/exercise/ExerciseThree.java
@@ -0,0 +1,19 @@
+package loiane.exercise;
+
+import java.util.Scanner;
+
+public class ExerciseThree {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println("Enter a number: ");
+        int numberOne = scan.nextInt();
+
+        System.out.println("Enter another number: ");
+        int numberTwo = scan.nextInt();
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("The sum of the numbers is " + sum);
+    }
+}

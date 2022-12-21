9eb69668e1223dd7aab99b2b1ff78c9b3ab67170
Marco Custodio <marcoantoniocp.71@gmail.com>
2022-12-21 18:09:11 -0300

    added feature/exercise-nine

---
+++ src/loiane/exercise/ExerciseNine.java
@@ -0,0 +1,15 @@
+package loiane.exercise;
+
+import java.util.Scanner;
+
+public class ExerciseNine {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the temperature in Fahrenheit: ");
+        float fah = scan.nextFloat();
+        float cel = (5* (fah-32) /9);
+
+        System.out.printf("The temperature in degrees celsius is %.2f", cel);
+    }
+}

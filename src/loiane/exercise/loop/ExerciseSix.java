d2209f21e48f49f2f92919a25292557d8bae95ba
Marco Custodio <marcoantoniocp.71@gmail.com>
2023-01-11 17:30:35 -0300

    added exercise-six

---
+++ src/loiane/exercise/loop/ExerciseSix.java
@@ -0,0 +1,12 @@
+package loiane.exercise.loop;
+
+public class ExerciseSix {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++){
+        System.out.println(i);
+        }
+        for (int i = 1; i <= 20; i++){
+            System.out.print(" "+i);
+        }
+    }
+}
